9b029d3bf8c27f874ae1280abd41ad68ca4b9001
PavanTN-CTS <pavan.n4@cognizant.com>
2025-07-10 18:26:21 +0530

    Home page Object pushed

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -1,6 +1,9 @@
 package pageObjects;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends BasePage {
 	
@@ -10,5 +13,55 @@ public class HomePage extends BasePage {
 		super(driver);
 	}
 	
+
+	//Locators
+
+		//For Insurance Products
+		@FindBy(xpath="//ul[@class='ruby-menu']/li[2]")
+		WebElement insuranceProductsButton;
+
+
+		//Travel Insurance
+		@FindBy(xpath="//section[@class='prd-row']/div[7]/a")
+		WebElement travelInsurance;
+
+
+		//Car Insurance
+		@FindBy(xpath="//section[@class='prd-row']/div[4]/a")
+		WebElement carInsurance;
+
+
+		//Health Insurance
+		@FindBy(xpath="//div[@class='ruby-col-3 hidden-md']//span[text()='Health Insurance Companies']/parent::a")
+		WebElement healthInsurance;
+
+
+
+
+		//Action Methods
+
+		//Hovering on the Insurance product
+		public void HoverOnInsuranceProducts() {
+			Actions act=new Actions(driver);
+			act.moveToElement(insuranceProductsButton).perform();
+		}
+
+
+		//Selecting the Health insurance 
+		public void healthInsuranceClick() {
+			Actions act=new Actions(driver);
+			act.doubleClick(healthInsurance).perform();
+		}
+
+		//Navigating to the Car Insurance Page
+		public void carInsuranceClick() {
+			carInsurance.click();
+		}
+
+		//Navigating to the Travel Insurance Page
+		public void travelInsurance() {
+			travelInsurance.click();
+		}
+
 
 }
